3c1f9471a39010f3fe1a9c6ef46cb7fe3947a17e
carlos6199 <carlos_fig14@hotmail.com>
2023-12-01 11:56:37 -0500

    backend works with postman

--- backend/daytracker/daytrackerapp/models.py
+++ backend/daytracker/daytrackerapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User, Group
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=50)
+    notes = models.CharField(max_length=200)
+    timestart = models.IntegerField()
+    timeend = models.IntegerField()
--- backend/daytracker/daytrackerapp/views.py
+++ backend/daytracker/daytrackerapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .models import Task
+from rest_framework import viewsets, permissions
+from .serializers import TaskSerializer
 # Create your views here.
+
+
+class DayTrackerViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_class = [permissions.AllowAny]
+
