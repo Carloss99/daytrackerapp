fb97877eefa2d9f6ece54a4817206cf2df359675
carlos6199 <carlos_fig14@hotmail.com>
2023-12-03 14:00:55 -0500

    updated task model

--- backend/daytracker/daytrackerapp/models.py
+++ backend/daytracker/daytrackerapp/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User, Group
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 class Task(models.Model):
     title = models.CharField(max_length=50)
-    notes = models.CharField(max_length=200)
-    timestart = models.IntegerField()
-    timeend = models.IntegerField()
+    notes = ArrayField(models.CharField(max_length=200))
+    timestart = models.CharField(max_length=7)
+    timeend = models.CharField(max_length=7)
